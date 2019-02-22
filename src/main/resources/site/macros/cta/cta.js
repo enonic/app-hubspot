81023f1c0221a4e4b0afb7d74f6625835f447980
Bjørnar Hjelmevold <bhj@enonic.com>
2019-02-22 11:40:33 +0100

    reduced cta margin to 30px

--- src/main/resources/site/macros/cta/cta.js
+++ src/main/resources/site/macros/cta/cta.js
@@ -13,7 +13,7 @@ exports.macro = function (context) {
         return {
             body: ctaContent.data.embedCode,
             pageContributions: {
-                headEnd: '<style type="text/css">.hs-cta-wrapper a{border:0}.body .hs-cta-wrapper{margin:60px 0;display:block;}</style>'
+                headEnd: '<style type="text/css">.hs-cta-wrapper a{border:0}.body .hs-cta-wrapper{margin:30px 0;display:block;}</style>'
             }
         }
     }
