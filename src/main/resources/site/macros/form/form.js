3b39291b068f1de3262a1dc717f43bd47f989b9a
Persijn kwekkeboom <3340349+poi33@users.noreply.github.com>
2023-05-04 10:00:06 +0200

    Form cty updated #17 (#24)
    * Form cty updated #17

--- src/main/resources/site/macros/form/form.js
+++ src/main/resources/site/macros/form/form.js
@@ -10,8 +10,28 @@ exports.macro = function (context) {
             key: formContentId
         });
 
+        let body = "";
+
+        const formData = formContent.data;
+
+        if (formData.formId && formData.portalId) {
+            body = `<script charset="utf-8" type="text/javascript"
+            src="//js.hsforms.net/forms/embed/v2.js"></script>
+            <script>
+                hbspt.forms.create({
+                    region: "${formData.regionName}",
+                    portalId: "${formData.portalId}",
+                    formId: "${formData.formId}"
+                });
+            </script>`;
+        // Old embed code for v1 forms
+        } else if (formData.embedCode) {
+            log.info("embed");
+            body = formData.embedCode
+        }
+
         return {
-            body: formContent.data.embedCode,
+            body: body,
             pageContributions: {
                 headEnd: '<style type="text/css">.hbspt-form .hs-form ul{list-style-image:none;list-style-type:none;margin:0;padding-left:0;}.hbspt-form{margin:0 0 60px 0;font-size:16px;}.body .hbspt-form{margin:60px 0;}.hs-form{border:1px solid #ddd;padding:15px 30px;}.hs-form-field{margin:15px 0;}.hs-form input[type=submit]{background:#fc5d4b;color:#fff;border:0;border-radius:5px;padding:10px 21px;margin:15px 0;}.hs-form input[type=submit]:hover,.hs-form input[type=submit]:focus{background:#fc4632}.hs-form input[type=radio]{margin-right:5px;}.hs-form label,.hs-form select{margin:5px 0;cursor:pointer;}.hs-form input[type=text], .hs-form input[type=email]{width:100%;padding:5px;max-width:400px;}.hs-form textarea{width:100%;padding:5px;min-height:6em;border:1px solid #ddd;}.hs-form-required{color:#fc4632;}</style>'
             }
